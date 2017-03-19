c921bb0248df15a1b3defd5b63bb1ce2c88d7e72
mohbou <a.bourkaib@gmail.com>
2017-03-19 15:03:12 -0400

    Initial commit

---
+++ app/src/test/java/com/quizappexam/app/quizappexam/test/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.quizappexam.app.quizappexam.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+
+    @Test
+    public void testShouldPass() {
+        assertEquals(1,1);
+
+    }
+
+}
