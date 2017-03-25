92c6d3e3df2bfe269b9bd0ab5c947618ec9a094c
mohbou <a.bourkaib@gmail.com>
2017-03-25 09:40:24 -0400

    commit

--- app/src/main/java/com/quizappexam/app/quizappexam/data/database/Database.java
+++ app/src/main/java/com/quizappexam/app/quizappexam/data/database/Database.java
@@ -1,4 +1,12 @@
 package com.quizappexam.app.quizappexam.data.database;
 
+import com.quizappexam.app.quizappexam.model.entity.Answer;
+import com.quizappexam.app.quizappexam.model.entity.Question;
+
+import java.util.List;
+
 public interface Database {
+    public List<Question> loadQuestion();
+    public Question getQuestionById(int Id);
+    public List<Answer> getAnswersNyId(int QuestionId);
 }
--- app/src/main/java/com/quizappexam/app/quizappexam/data/database/DatabaseImpl.java
+++ app/src/main/java/com/quizappexam/app/quizappexam/data/database/DatabaseImpl.java
@@ -1,4 +1,23 @@
 package com.quizappexam.app.quizappexam.data.database;
 
+import com.quizappexam.app.quizappexam.model.entity.Answer;
+import com.quizappexam.app.quizappexam.model.entity.Question;
+
+import java.util.List;
+
 public class DatabaseImpl implements Database {
+    @Override
+    public List<Question> loadQuestion() {
+        return null;
+    }
+
+    @Override
+    public Question getQuestionById(int Id) {
+        return null;
+    }
+
+    @Override
+    public List<Answer> getAnswersNyId(int QuestionId) {
+        return null;
+    }
 }
