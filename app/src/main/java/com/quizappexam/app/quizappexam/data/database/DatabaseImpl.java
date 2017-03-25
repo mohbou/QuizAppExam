0ee2128e9955d99ec123e9427880b57083e6c882
mohbou <a.bourkaib@gmail.com>
2017-03-25 12:16:46 -0400

    commit

--- app/src/main/java/com/quizappexam/app/quizappexam/data/database/DatabaseImpl.java
+++ app/src/main/java/com/quizappexam/app/quizappexam/data/database/DatabaseImpl.java
@@ -3,13 +3,16 @@ package com.quizappexam.app.quizappexam.data.database;
 import com.quizappexam.app.quizappexam.model.entity.Answer;
 import com.quizappexam.app.quizappexam.model.entity.Question;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class DatabaseImpl implements Database {
 
 private static DatabaseImpl DBInstance;
-
+    private List<Question> mQuestions;
+    private List<Answer> mAnswers;
     private DatabaseImpl() {
+        initDummyQuestions();
     }
 
 
@@ -19,17 +22,39 @@ private static DatabaseImpl DBInstance;
 
     @Override
     public List<Question> loadQuestion() {
-        return null;
+        return mQuestions;
     }
 
     @Override
-    public Question getQuestionById(int Id) {
-        return null;
+    public Question getQuestionById(int id) {
+        return mQuestions.get(id);
     }
 
     @Override
     public List<Answer> getAnswersNyId(int QuestionId) {
         return null;
+    }
+
+    private void initDummyQuestions() {
+
+        mQuestions = new ArrayList<>();
+
+        for(int i=0;i<20;i++) {
+            Question question = new Question();
+            mAnswers = new ArrayList<>();
+            question.setQuestion("This is a dummy Question number "+i);
+
+            for(int j=0;j<4;j++) {
+                Answer answer =  new Answer();
+                answer.setAnswerID(j);
+                answer.setAnswerString("Dummy Answer number "+j);
+                answer.setAnswerValid(true);
+                mAnswers.add(answer);
+            }
+            question.setAnswerList(mAnswers);
+            mQuestions.add(question);
+
+        }
     }
 
 
