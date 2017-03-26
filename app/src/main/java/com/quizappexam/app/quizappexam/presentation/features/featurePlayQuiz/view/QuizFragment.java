6afa7f901f8583c2fb4a1786c6205e2e10a92047
mohbou <a.bourkaib@gmail.com>
2017-03-26 19:43:21 -0400

    commit

--- app/src/main/java/com/quizappexam/app/quizappexam/presentation/features/featurePlayQuiz/view/QuizFragment.java
+++ app/src/main/java/com/quizappexam/app/quizappexam/presentation/features/featurePlayQuiz/view/QuizFragment.java
@@ -3,6 +3,7 @@ package com.quizappexam.app.quizappexam.presentation.features.featurePlayQuiz.vi
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,7 @@ public class QuizFragment extends Fragment{
 
 
     @BindView(R.id.recycler_view_answers)
-    RecyclerView mRecyclerView;
+    RecyclerView mAnswerRecyclerView;
 
     @Nullable
     @Override
@@ -25,6 +26,36 @@ public class QuizFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_quiz_question, container, false);
         ButterKnife.bind(this,view);
+        mAnswerRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return view;
+    }
+
+
+
+    private class AnswerViewHolder extends RecyclerView.ViewHolder {
+
+
+        public AnswerViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private class AnswerAdapter extends RecyclerView.Adapter<AnswerViewHolder> {
+
+
+        @Override
+        public AnswerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(AnswerViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
